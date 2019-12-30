5378ff29195c5b37e83200d598f684092eee9507
iuriev <ivan.iuriev@gmail.com>
2019-12-30 11:30:06 +0200

    delete console.log etc

--- app.js
+++ app.js
@@ -1,76 +1,59 @@
 const bodyParser = require("body-parser");
 const {Client} = require('pg');
-
-
-
 const connectionString = 'postgres://postgres:admin@localhost:5432/guru99';
 const client = new Client({connectionString: connectionString});
 const express = require('express');
-
 const app = express();
 app.use(bodyParser.json());
-
 app.use(bodyParser.urlencoded({extended: true}));
 client.connect();
 
 app.listen(3001, function () {
-  console.log('Server started on port 3001 ..')
+    console.log('Server started on port 3001 ..')
 });
 
-
 app.get('/', function (req, res) {
-
- // var sql = `SELECT * FROM account ORDER BY user_id ASC;`;
-  var sql = `SELECT * FROM account;`;
-  client.query(sql, (err, queryRes) => {
-    if (err) {
-      console.log("node eeror");
-      throw err;
-    }
-    console.log("in node");
-    res.json(queryRes.rows);
-  });
+    var sql = `SELECT * FROM account ORDER BY user_id ASC;`;
+    client.query(sql, (err, queryRes) => {
+        if (err) {
+            throw err;
+        }
+        res.json(queryRes.rows);
+    });
 });
 
 app.post('/create', function (req, res) {
-  var sql = `INSERT INTO account (fn,ln,age, ht) VALUES ($1, $2,$3, $4);`;
-
-  client.query(sql, [req.body.fn, req.body.ln, req.body.age, req.body.age], (err, res) => {
-    if (err) {
-      throw err;
-    }
-  });
-
-  console.log('Student ' + req.body.fn + ' ' + req.body.ln + ' was created.');
-  res.send('Student ' + req.body.fn + ' ' + req.body.ln + ' was created.');
+    var sql = `INSERT INTO account (fn,ln,age, ht) VALUES ($1, $2,$3, $4);`;
+    client.query(sql, [req.body.fn, req.body.ln, req.body.age, req.body.age], (err, res) => {
+        if (err) {
+            throw err;
+        }
+    });
+    res.send('Student ' + req.body.fn + ' ' + req.body.ln + ' was created.');
 });
 
 app.post('/update', function (req, res) {
-
-  var sql = `UPDATE account SET fn = $2, ln = $3, age = $4, ht = $5 WHERE user_id = $1;`;
-  client.query(sql, [req.body.id, req.body.fn, req.body.ln, req.body.age, req.body.ht], function (err, res) {
-    if (err) {
-      throw err;
-    }
-  });
-  console.log("Information about student with id = " + req.body.id + " was update.");
-  res.json({
-    "id": req.body.id,
-    "fn": req.body.id,
-    "ln": req.body.ln,
-    "age": req.body.age,
-    "ht": req.body.ht
-  })
+    var sql = `UPDATE account SET fn = $2, ln = $3, age = $4, ht = $5 WHERE user_id = $1;`;
+    client.query(sql, [req.body.id, req.body.fn, req.body.ln, req.body.age, req.body.ht], function (err, res) {
+        if (err) {
+            throw err;
+        }
+    });
+    res.json({
+        "id": req.body.id,
+        "fn": req.body.id,
+        "ln": req.body.ln,
+        "age": req.body.age,
+        "ht": req.body.ht
+    })
 });
 
 app.post('/delete', function (req, res) {
-  console.log ("helo node");
-  var sql = `DELETE FROM account WHERE user_id = $1 `;
-  client.query(sql, [req.body.id], (err, res) => {
-    if (err) {
-      throw err;
-    }
-  });
-  console.log("Student with id = " + req.body.id + " was deleted.");
-  res.send("Student with id = " + req.body.id + " was deleted.");
+    var sql = `DELETE FROM account WHERE user_id = $1 `;
+    client.query(sql, [req.body.id], (err, res) => {
+        if (err) {
+            throw err;
+        }
+    });
+    res.send("Student with id = " + req.body.id + " was deleted.");
 });
--- js/index.js
+++ js/index.js
@@ -1,10 +1,5 @@
 window.onload = function () {
-     importStudent();
-    //sortStudents();
-    // mapStudents();
-    // filterStudents();
-    //reduceStudents ();
-    //foreachStudents();
+    importStudent();
     document.getElementById('btnRead').disabled = true;
 }
 
@@ -27,19 +22,17 @@ function drawtable(temp) {
             }
         }
     }
-    // CREATE DYNAMIC TABLE.
     var table = document.createElement("table");
     table.classList.add('table__dynamic')
-    // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
-    var tr = table.insertRow(-1);                  // TABLE ROW.
+
+    var tr = table.insertRow(-1);
     for (var i = 0; i < col.length; i++) {
-        var th = document.createElement("th");      // TABLE HEADER.
+        var th = document.createElement("th");
         th.classList.add('th')
         th.innerHTML = col[i];
         tr.appendChild(th);
     }
 
-    // ADD JSON DATA TO THE TABLE AS ROWS.
     for (var i = 0; i < temp.length; i++) {
         tr = table.insertRow(-1);
         for (var j = 0; j < col.length; j++) {
@@ -48,7 +41,6 @@ function drawtable(temp) {
             tabCell.innerHTML = temp[i][col[j]];
         }
     }
-    // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
     var divContainer = document.getElementById("showData");
     divContainer.innerHTML = "";
     divContainer.appendChild(table);
@@ -75,15 +67,12 @@ function createStudent() {
 
     if (fn === "" || ln === "" || age === "" || ht === "") {
         alert("Field must be field");
-
     } else {
         var xhr = new XMLHttpRequest();
         xhr.open("POST", 'http://localhost:3001/create', true);
         xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         xhr.onreadystatechange = function () {//Вызывает функцию при смене состояния.
             if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-                //  var temp = JSON.parse(xhr.responseText);
-                //  console.log(temp);
             }
         }
         xhr.send("fn=" + fn + "&ln=" + ln + "&age=" + age + "&ht=" + ht);
@@ -107,8 +96,6 @@ function updateStudent() {
         xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         xhr.onreadystatechange = function () {//Вызывает функцию при смене состояния.
             if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-                //    var temp = JSON.parse(xhr.responseText);
-                //    console.log(temp);
             }
         }
         xhr.send("id=" + id + "&fn=" + fn + "&ln=" + ln + "&age=" + age + "&ht=" + ht);
@@ -118,10 +105,8 @@ function updateStudent() {
 
 function deleteStudent() {
     var id = document.getElementById('studentId').value;
-    //  console.log
     if (id === "") {
         alert("Field ID is empty");
-
     } else {
         var xhr = new XMLHttpRequest();
         xhr.open("POST", 'http://localhost:3001/delete', true);
@@ -135,116 +120,6 @@ function deleteStudent() {
     }
 }
 
-function sortStudents() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", 'http://localhost:3001/', true);
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function () {//Вызывает функцию при смене состояния.
-        if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-            var temp = JSON.parse(xhr.responseText);
-            temp.sort(function (a, b) {
-                if (a.ln > b.ln) {
-                    return 1;
-                } else {
-                    return -1;
-                }
-            });
-            drawtable(temp);
-        }
-    }
-    xhr.send();
-}
-
-function mapStudents() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", 'http://localhost:3001/', true);
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function () {//Вызывает функцию при смене состояния.
-        if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-            var temp = JSON.parse(xhr.responseText);
-
-            //  var res = temp.map(function(a){
-
-            var res = temp;
-
-            var res = temp.map(function (a) {
-                return
-                a.ln.toUpperCase();
-            });
-
-            console.log(res);
-
-            drawtable(temp);
-        }
-    }
-    xhr.send();
-}
-
-function filterStudents() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", 'http://localhost:3001/', true);
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function () {//Вызывает функцию при смене состояния.
-        if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-
-
-            var temp = JSON.parse(xhr.responseText);
-
-            var res = temp.filter(function (a) {
-                return a.age > 25;
-            });
-
-            console.log(res);
-
-            drawtable(res);
-        }
-    }
-    xhr.send();
-}
-
-function reduceStudents() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", 'http://localhost:3001/', true);
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function () {//Вызывает функцию при смене состояния.
-        if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-
-            var temp = JSON.parse(xhr.responseText);
-
-            var t = temp.reduce((prev, item) => prev + item.ln.length, 0);
-
-
-            drawtable(temp);
-            console.log(t);
-        }
-    }
-    xhr.send();
-}
-
-function foreachStudents() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", 'http://localhost:3001/', true);
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function () {//Вызывает функцию при смене состояния.
-        if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-
-            var temp = JSON.parse(xhr.responseText);
-
-            temp.forEach(function (slide) {
-
-                var node = document.createElement("dt");
-                var textnode = document.createTextNode("name");
-                var node2 = document.createElement("dd");  // Create a <li> node
-                var textnode2 = document.createTextNode(slide.ln);
-                node.appendChild(textnode);// Create a text node
-                node2.appendChild(textnode2);                              // Append the text to <li>
-                document.getElementById("myDl").appendChild(node);     // Append <li> to <ul> with id="myList"
-                document.getElementById("myDl").appendChild(node2);
-            });
-        }
-    }
-    xhr.send();
-}
 
 
 
