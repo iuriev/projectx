48d58fccb02f873cc39b7693658dfb967f1788be
iuriev <ivan.iuriev@gmail.com>
2020-01-10 11:32:24 +0200

    add teacher roating functions

--- app.js
+++ app.js
@@ -18,7 +18,43 @@ app.use(function (req, res, next) {
 app.listen(3001, function () {
     console.log('Server started on port 3001 ..')
 });
+//////////////////////////////////////////////////////////////////////////////////
+// new functions
+app.post('/getStudents', function (req, res) {
 
+    var sql = `SELECT * FROM account WHERE teacher_id = $1;`;
+    client.query(sql,  [req.body.id],(err, response) => {
+        if (err) {
+            res.status(200).send("ID ERROR");
+        } else {
+             res.json(response.rows);
+        }
+    });
+});
+
+app.post('/createTeacher', function (req, res) {
+    var sql = `INSERT INTO teacher (id,login,password,email,phone) VALUES ($1, $2,$3, $4, $5) ;`;
+    client.query(sql, [req.body.id, req.body.login, req.body.password, req.body.email, req.body.email], (err, response) => {
+        if (err) {
+            res.status(200).send("ID ERROR");
+        } else {
+            res.status(200).send();
+        }
+    });
+});
+
+app.post('/login', function (req, res) {
+    var sql = `SELECT * FROM teacher WHERE login = $1 AND password = $2;`;
+    client.query(sql, [req.body.login,  req.body.password], (err, response) => {
+        if (err) {
+            res.status(200).send("ID ERROR");
+        } else {
+            res.status(200).send();
+        }
+    });
+});
+
+////////////////////////////////////////////////////////////////////////////////////////
 app.get('/getAllStudents', function (req, res) {
     var sql = `SELECT * FROM account ORDER BY id ASC;`;
     client.query(sql, (err, queryRes) => {
