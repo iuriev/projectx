6d4157d52302d31512b0c30e6032d4be185f2e77
iuriev <ivan.iuriev@gmail.com>
2020-01-16 21:24:00 +0200

    fix update bug
    fix user login describe bug

--- src/account.js
+++ src/account.js
@@ -50,10 +50,11 @@ function updateInfo() {
         xhr.setRequestHeader("Content-type", "application/json");
         xhr.onreadystatechange = function () {
             if (xhr.readyState === 4 && xhr.status === 200) {
-                importInputs();  
+                importInputs();
+                localStorage.setItem( "UserLogin",login.value);
             }
         };
-        
+
         xhr.send(JSON.stringify(requestBody));
     }
 }
