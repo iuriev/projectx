18142829b5dbaf4eb272aaf69c6660549c67e494
iuriev <ivan.iuriev@gmail.com>
2020-01-25 15:46:59 +0200

    change DB for working with groups
    add tabs function

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,6 @@
 var config = require('../static/config.js');
 var helper = require('../helpers/helper.js');
-var bodyParser = require("body-parser");
+var bodyParser = require('body-parser');
 var {Client} = require('pg');
 var express = require('express');
 var server = express();
@@ -20,7 +20,7 @@ server.listen(3001, function () {
 
 server.post('/authorize-teacher', function (req, res) {
 
-    var sql = `SELECT id,login FROM teacher WHERE login = $1 AND password = $2;`;
+    var sql = `SELECT id,login FROM teacher WHERE login = $1 AND password = $2 ORDER BY id ASC; `;
     client.query(sql, [req.body.login, req.body.password], (error, response) => {
         if (error) {
             res.status(500).send("SERVER ERROR");
@@ -35,19 +35,50 @@ server.post('/authorize-teacher', function (req, res) {
 });
 
 server.post('/create-teacher', function (req, res) {
-    var sql = `INSERT INTO teacher (login,password,email,phone) VALUES ($1, $2, $3, $4) ;`;
-    client.query(sql, [req.body.login, req.body.password, req.body.email, req.body.phone], (err, response) => {
+    var newGroupId = 0;
+    var newTeacherId = 0;
+    var sql1 = `INSERT INTO teacher (login,password,email,phone) VALUES ($1, $2, $3, $4) RETURNING id;`;
+    client.query(sql1, [req.body.login, req.body.password, req.body.email, req.body.phone], (err, response) => {
         if (err) {
             res.status(502).send("SERVER ERROR");
         } else {
-            res.status(200).send();
+            newTeacherId = response.rows[0].id;
+            var sql2 = `INSERT INTO course_group (name) VALUES ($1) RETURNING id ;`;
+            client.query(sql2, ["untitled"], (err, response) => {
+                if (err) {
+                    res.status(502).send("SERVER ERROR");
+                } else {
+                    newGroupId = response.rows[0].id;
+                    var sql = `INSERT INTO course_teacher (id_teacher , id_group) VALUES ($1, $2);`;
+                    client.query(sql, [newTeacherId, newGroupId], (err, response) => {
+                        if (err) {
+                            res.status(502).send("SERVER ERROR");
+                        } else {
+                            res.status(200).send();
+                        }
+                    });
+                }
+            });
+        }
+    });
+});
+
+server.get('/teacher-groups', function (req, res) {
+
+    var sql = `SELECT t1.id_group, t2.name FROM course_teacher t1 inner join course_group t2 on t1.id_group = t2.id WHERE id_teacher = $1 ORDER BY t1.id ASC ;`;
+    client.query(sql, [req.query.id_teacher], (err, response) => {
+        if (err) {
+            res.status(500).send("SERVER ERROR");
+        } else {
+            console.log(response.rows);
+            res.status(200).json(response.rows);
         }
     });
 });
 
 server.get('/all-students', function (req, res) {
-    var sql = `SELECT id,fn,ln,age,ht FROM account WHERE teacher_id = $1 ORDER BY id ASC;`;
-    client.query(sql, [req.query.id], (err, response) => {
+    var sql = `SELECT id,fn,ln,age,ht FROM account WHERE id_group = $1 ORDER BY id ASC;`;
+    client.query(sql, [req.query.id_group], (err, response) => {
         if (err) {
             res.status(500).send("SERVER ERROR");
         } else {
@@ -57,7 +88,7 @@ server.get('/all-students', function (req, res) {
 });
 
 server.get('/teacher', function (req, res) {
-    var sql = `SELECT login,password,email,phone FROM teacher WHERE id = $1;`;
+    var sql = `SELECT login,password,email,phone FROM teacher WHERE id = $1 ORDER BY id ASC;`;
     client.query(sql, [req.query.id], (err, response) => {
         if (err) {
             res.status(500).send("SERVER ERROR");
@@ -78,9 +109,20 @@ server.post('/update-teacher', function (req, res) {
     });
 });
 
+server.post('/save-group-name', function (req, res) {
+    var sql = `UPDATE course_group SET name = $2 WHERE id =$1;`;
+    client.query(sql, [req.body.id, req.body.name ], (err, response) => {
+        if (err) {
+            res.status(500).send("SERVER ERROR");
+        } else {
+            res.status(200).send();
+        }
+    });
+});
+
 server.post('/create-student', function (req, res) {
-    var sql = `INSERT INTO account (fn,ln,age,ht,teacher_id) VALUES ($1, $2,$3, $4, $5) ;`;
-    client.query(sql, [req.body.fn, req.body.ln, req.body.age, req.body.ht, req.body.teacherId], (err, response) => {
+    var sql = `INSERT INTO account (fn,ln,age,ht,id_group) VALUES ($1, $2,$3, $4, $5) ;`;
+    client.query(sql, [req.body.fn, req.body.ln, req.body.age, req.body.ht, req.body.id_group], (err, response) => {
         if (err) {
             res.status(500).send("SERVER ERROR");
         } else {
@@ -110,3 +152,55 @@ server.post('/delete-student', function (req, res) {
         }
     });
 });
+
+server.post('/delete-group', function (req, res) {
+    var sql = `DELETE FROM course_teacher WHERE id_group = $1 `;
+    client.query(sql, [req.body.id], (err, response) => {
+        if (err) {
+            res.status(501).send("SERVER ERROR");
+        } else {
+            var sql2 = `DELETE FROM account WHERE id_group = $1 `;
+            client.query(sql2, [req.body.id], (err, response) => {
+                if (err) {
+                    res.status(502).send("SERVER ERROR");
+                } else {
+                    var sql3 = `DELETE FROM course_group WHERE id = $1 `;
+                    client.query(sql3, [req.body.id], (err, response) => {
+                        if (err) {
+                            res.status(503).send("SERVER ERROR");
+                        } else {
+                            res.status(200).send();
+                        }
+                    });
+                }
+            });
+        }
+    });
+});
+
+
+server.post('/create-new-group', function (req, res) {
+
+    console.log("ffd")
+    var newGroupId = 0;
+    var sql2 = `INSERT INTO course_group (name) VALUES ($1) RETURNING id ;`;
+    client.query(sql2, ["untitled"], (err, response) => {
+        if (err) {
+            res.status(502).send("SERVER ERROR");
+        } else {
+            newGroupId = response.rows[0].id;
+            var sql = `INSERT INTO course_teacher (id_teacher , id_group) VALUES ($1, $2);`;
+            client.query(sql, [req.body.id, newGroupId], (err, response) => {
+                if (err) {
+                    res.status(502).send("SERVER ERROR");
+                } else {
+                    res.status(200).send();
+                }
+            });
+        }
+    });
+
+
+});
+
+
--- src/students.js
+++ src/students.js
@@ -7,8 +7,9 @@ let fn;
 let ln;
 let age;
 let ht;
-window.addEventListener('load', function () {
 
+window.addEventListener('load', function () {
+    getTeacherGroups();
     fn = document.getElementById('firstName');
     ln = document.getElementById('lastName');
     age = document.getElementById('age');
@@ -16,26 +17,215 @@ window.addEventListener('load', function () {
     let languageArray = helpers.getCurrentLanguagesSet();
     document.getElementById('language-authorization').addEventListener('change', helpers.changeSelectedValue);
     document.getElementById('myLoginName').innerHTML = languageArray.s_teacher_login + localStorage.getItem("UserLogin");
-    document.getElementById('btnCreate').addEventListener('click', function () {
+    document.getElementById('fullScreen').addEventListener('click', fullScreen);
+    document.getElementById('logout').addEventListener('click', logout);
 
+    document.getElementById('btnCreate').addEventListener('click', function () {
         if (utils.validateCreateInput(fn.value, ln.value, ht.value, age.value)) {
             createStudent()
         }
-
     });
-    document.getElementById('fullScreen').addEventListener('click', fullScreen);
-    document.getElementById('logout').addEventListener('click', logout);
+
     document.getElementById('account').addEventListener('click', function () {
         utils.changeLocation(constants.pathAccount);
     });
-    let e = document.getElementById("language-authorization");
-    localStorage.getItem('language') === "RU" ?  e.selectedIndex = 0 : e.selectedIndex = 1;
-    helpers.setStudentsPageLanguage();
-    importStudent();
+
+    changeStudentsPageLanguage();
+    createContainers();
+    drawtable();
+    addTabsOnStudentsPage();
 });
 
+function changeStudentsPageLanguage() {
+    let e = document.getElementById("language-authorization");
+    localStorage.getItem('language') === "RU" ? e.selectedIndex = 0 : e.selectedIndex = 1;
+    helpers.setStudentsPageLanguage();
+}
+
+function addTabsOnStudentsPage() {
+
+    let arr = JSON.parse(localStorage.getItem("teacherGroups"));
+    if (arr.length < 3) {
+        document.getElementById("addNewTab").addEventListener('click', createNewGroup);
+    }
+
+    let classname1 = document.getElementsByClassName("tabs__link");
+    for (let i = 0; i < classname1.length; i++) {
+        classname1[i].addEventListener('click', changetab, false);
+        classname1[i].addEventListener('dblclick', changeTabName, false);
+    }
+
+    let classname = document.getElementsByClassName("deleteTabButton");
+    for (let i = 0; i < classname.length; i++) {
+
+        classname[i].addEventListener('click', deleteTab, false);
+    }
+}
+
+function changeTabName() {
+    let oldTab = this;
+    let oldTabText = this.innerText;
+    let oldTabId = this.id;
+    let tabIndex = oldTabId[7];
+    let tabClass = this.classList;
+    let parent = this.parentNode;
+    let input = document.createElement('input');
+    let div = document.createElement('div');
+    div.id = oldTabId;
+    div.classList = tabClass;
+    input.id = "inputTabName";
+    input.value = oldTabText;
+    parent.replaceChild(input, this);
+    input.focus();
+    input.onblur = function () {
+        let newText = input.value;
+        oldTab.innerText = newText;
+
+        let deleteButton = document.createElement("button");
+        deleteButton.id = "deleteButton" + tabIndex;
+        deleteButton.classList.add("deleteTabButton");
+        deleteButton.addEventListener('click', deleteTab, false);
+
+        parent.replaceChild(oldTab, this);
+        oldTab.append(deleteButton);
+        let arr = JSON.parse(localStorage.getItem("teacherGroups"));
+        let arrNames = JSON.parse(localStorage.getItem("teacherGroupsNames"));
+        arrNames[tabIndex - 1] = newText;
+        localStorage.setItem("teacherGroupsNames", arrNames)
+        let requestBody = {
+            id: arr[tabIndex - 1],
+            name: newText
+        };
+
+        xhr.open("POST", `${constants.server}save-group-name`, false);
+        xhr.setRequestHeader("Content-type", "application/json");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                getTeacherGroups();
+                cansel();
+            }
+        };
+        xhr.send(JSON.stringify(requestBody));
+
+    };
+}
+
+function deleteTab() {
+
+    var arr = JSON.parse(localStorage.getItem("teacherGroups"));
+    var tab_id = this.id.slice(-1);
+    var id = arr[tab_id - 1];
+    var requestBody = {
+        id: id
+    };
+    xhr.open("POST", `${constants.server}delete-group`, false);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            getTeacherGroups();
+            cansel();
+        }
+    };
+    xhr.send(JSON.stringify(requestBody));
+}
+
+function createNewGroup() {
+
+    var requestBody = {
+        id: localStorage.getItem("UserID")
+    };
+    xhr.open("POST", `${constants.server}create-new-group`, false);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            getTeacherGroups();
+            cansel();
+        }
+    };
+    xhr.send(JSON.stringify(requestBody));
+}
+
+
+function changetab() {
+    let tab2 = document.getElementById("content-2");
+    let tab3 = document.getElementById("content-3");
+
+    switch (this.id[7]) {
+        case '1':
+            localStorage.setItem("activeTab", 1);
+            document.getElementById("content-1").classList.add("tabs__pane_show");
+            if (tab2) {
+                tab2.classList.remove("tabs__pane_show");
+                document.getElementById("content2").classList.remove('tabs__link_active');
+            }
+            if (tab3) {
+                tab3.classList.remove("tabs__pane_show");
+                document.getElementById("content3").classList.remove('tabs__link_active');
+            }
+            document.getElementById("content1").classList.add('tabs__link_active');
+            break;
+        case '2':
+            localStorage.setItem("activeTab", 2);
+            document.getElementById("content-1").classList.remove("tabs__pane_show");
+            document.getElementById("content-2").classList.add("tabs__pane_show");
+            document.getElementById("content1").classList.remove('tabs__link_active');
+            document.getElementById("content2").classList.add('tabs__link_active');
+            if (tab3) {
+                tab3.classList.remove("tabs__pane_show");
+                document.getElementById("content3").classList.remove('tabs__link_active');
+            }
+            break;
+        case '3':
+            localStorage.setItem("activeTab", 3);
+            document.getElementById("content-1").classList.remove("tabs__pane_show");
+            document.getElementById("content-2").classList.remove("tabs__pane_show");
+            document.getElementById("content-3").classList.add("tabs__pane_show");
+            document.getElementById("content1").classList.remove('tabs__link_active');
+            document.getElementById("content2").classList.remove('tabs__link_active');
+            document.getElementById("content3").classList.add('tabs__link_active');
+    }
+}
+
+function createContainers() {
+
+    let arr = JSON.parse(localStorage.getItem("teacherGroups"));
+    let arrNames = JSON.parse(localStorage.getItem("teacherGroupsNames"));
+    let nav = document.getElementById('tab-nav');
+    let tableField = document.getElementById('tabs__content');
+    for (let a = 1; a < arr.length + 1; a++) {
+        let newTab = document.createElement('div');
+        if (a === 1) {
+            newTab.innerHTML = arrNames[a - 1]
+        } else {
+            newTab.innerHTML = arrNames[a - 1] + "<button id = 'deleteButton" + a + "' class='deleteTabButton'></button>";
+        }
+        newTab.href = "#content" + a;
+        newTab.id = "content" + a;
+        newTab.classList = "tabs__link";
+        nav.append(newTab);
+
+        let panel = document.createElement('div');
+        panel.id = "content-" + a;
+        panel.classList.add("tabs__pane");
+        let curr = localStorage.getItem("activeTab");
+        if (curr == a) {
+            newTab.classList = "tabs__link tabs__link_active";
+            panel.classList.add("tabs__pane_show");
+        }
+        tableField.append(panel);
+    }
+
+    if (arr.length < 3) {
+        let addButton = document.createElement('button');
+        addButton.innerText = "+";
+        addButton.classList = "add-new-tab";
+        addButton.id = "addNewTab";
+        nav.append(addButton);
+    }
+}
+
 function fullScreen() {
-    document.fullscreenElement ? document.exitFullscreen(): document.documentElement.requestFullscreen();
+    document.fullscreenElement ? document.exitFullscreen() : document.documentElement.requestFullscreen();
 }
 
 function logout() {
@@ -43,11 +233,11 @@ function logout() {
     utils.changeLocation(constants.pathAuthorizationPage);
 }
 
-function drawHeader() {
+function drawHeader(a) {
     let languageArray = helpers.getCurrentLanguagesSet();
     let col = [languageArray.s_fn, languageArray.s_ln, languageArray.s_age, languageArray.s_ht, languageArray.s_actions];
     let table = document.createElement("table");
-    table.id = "mainTable";
+    table.id = "mainTable" + a;
     table.classList.add('table__dynamic')
     let tr = table.insertRow(-1);
     for (let i = 0; i < col.length; i++) {
@@ -56,53 +246,57 @@ function drawHeader() {
         th.innerHTML = col[i];
         tr.appendChild(th);
     }
-    let divContainer = document.getElementById("showData");
+    let divContainer = document.getElementById("content-" + a);
     divContainer.innerHTML = "";
     divContainer.appendChild(table);
 }
 
-function drawtable(temp) {
-    drawHeader();
-    let col = [];
-    for (let i = 0; i < temp.length; i++) {
-        for (let key in temp[i]) {
-            if (col.indexOf(key) === -1) {
-                col.push(key);
+function drawtable() {
+    var arr = JSON.parse(localStorage.getItem("teacherGroups"));
+    for (var a = 1; a < arr.length + 1; a++) {
+        importStudent(arr[a - 1]);
+        var temp = JSON.parse(localStorage.getItem("tabledata" + arr[a - 1]));
+        drawHeader(a);
+        let col = [];
+        for (let i = 0; i < temp.length; i++) {
+            for (let key in temp[i]) {
+                if (col.indexOf(key) === -1) {
+                    col.push(key);
+                }
             }
         }
-    }
-    let table = document.getElementById("mainTable");
-    for (let k = 0; k < temp.length; k++) {
-        let tr = table.insertRow(-1);
-        for (let j = 1; j < col.length; j++) {
-            let tabCell = tr.insertCell(-1);
-            tabCell.classList.add('tabCell');
-            tabCell.id = col[j] + temp[k][col[0]];
-            tabCell.innerHTML = temp[k][col[j]];
+        let table = document.getElementById("mainTable" + a);
+        for (let k = 0; k < temp.length; k++) {
+            let tr = table.insertRow(-1);
+            for (let j = 1; j < col.length; j++) {
+                let tabCell = tr.insertCell(-1);
+                tabCell.classList.add('tabCell');
+                tabCell.id = col[j] + temp[k][col[0]];
+                tabCell.innerHTML = temp[k][col[j]];
+            }
+            let languageArray = helpers.getCurrentLanguagesSet();
+            let btnUpdate = document.createElement("button");
+            btnUpdate.id = "updateBtn" + temp[k][col[0]];
+            btnUpdate.innerHTML = languageArray.button_update;
+            btnUpdate.classList.add('update-btn');
+            btnUpdate.addEventListener('click', updatePreparation);
+            tr.appendChild(btnUpdate);
+
+            let btnDelete = document.createElement("button");
+            btnDelete.id = "deleteBtn" + temp[k][col[0]];
+            btnDelete.innerHTML = languageArray.button_delete;
+            btnDelete.classList.add('delete-btn');
+            btnDelete.addEventListener('click', deleteStudent);
+            tr.appendChild(btnDelete);
         }
-        let languageArray = helpers.getCurrentLanguagesSet();
-        let btnUpdate = document.createElement("button");
-        btnUpdate.id = "updateBtn" + temp[k][col[0]];
-        btnUpdate.innerHTML = languageArray.button_update;
-        btnUpdate.classList.add('update-btn');
-        btnUpdate.addEventListener('click', updatePreparation);
-        tr.appendChild(btnUpdate);
-
-        let btnDelete = document.createElement("button");
-        btnDelete.id = "deleteBtn" + temp[k][col[0]];
-        btnDelete.innerHTML = languageArray.button_delete;
-        btnDelete.classList.add('delete-btn');
-        btnDelete.addEventListener('click', deleteStudent);
-        tr.appendChild(btnDelete);
+        let divContainer = document.getElementById("content-" + a);
+        divContainer.innerHTML = "";
+        divContainer.appendChild(table);
     }
 
-    let divContainer = document.getElementById("showData");
-    divContainer.innerHTML = "";
-    divContainer.appendChild(table);
 }
 
 function updatePreparation() {
-
     let id = this.id.substring(9, this.id.length);
     let fn = document.getElementById('fn' + id);
     let fnText = fn.textContent;
@@ -139,34 +333,57 @@ function cansel() {
     document.location.reload();
 }
 
-function importStudent() {
+function getTeacherGroups() {
+    localStorage.setItem("activeTab", 1);
     let id = localStorage.getItem("UserID");
-    xhr.open("GET", `${constants.server}all-students?id=${id}`, true);
+    xhr.open("GET", `${constants.server}teacher-groups?id_teacher=${id}`, false);
     xhr.setRequestHeader("Content-type", "application/json");
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
+            var arrNumbers = [];
+            var arrNames = [];
             let temp = JSON.parse(xhr.responseText);
-            drawtable(temp);
+            for (let i = 0; i < temp.length; i++) {
+                arrNumbers.push(temp[i].id_group)
+                arrNames.push(temp[i].name)
+            }
+            localStorage.setItem("teacherGroups", JSON.stringify(arrNumbers));
+            localStorage.setItem("teacherGroupsNames", JSON.stringify(arrNames));
         }
     };
     xhr.send();
 }
 
-function createStudent() {
+function importStudent(id_group) {
+    xhr.open("GET", `${constants.server}all-students?id_group=${id_group}`, false);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var temp = JSON.parse(xhr.responseText);
+            localStorage.setItem("tabledata" + id_group, JSON.stringify(temp));
+        }
+    };
+    xhr.send();
 
-    var userID = localStorage.getItem("UserID");
+}
+
+function createStudent() {
+    var tab = document.getElementsByClassName('tabs__link_active');
+    var tab_id = tab[0].href.slice(-1);
+    var arr = JSON.parse(localStorage.getItem("teacherGroups"));
+    let id_group = arr[tab_id - 1];
     var requestBody = {
         fn: fn.value,
         ln: ln.value,
         age: age.value,
         ht: ht.value,
-        teacherId: userID
+        id_group: id_group
     };
     xhr.open("POST", `${constants.server}create-student`);
     xhr.setRequestHeader("Content-type", "application/json");
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
-            importStudent();
+            drawtable();
         }
     };
     xhr.send(JSON.stringify(requestBody));
@@ -182,11 +399,11 @@ function updateStudent() {
         ht: document.getElementById(`inputHt${id}`).value
     };
     if (utils.validateCreateInput(requestBody.fn, requestBody.ln, requestBody.ht, requestBody.age)) {
-        xhr.open("POST", `${constants.server}update-student-info`, true);
+        xhr.open("POST", `${constants.server}update-student-info`, false);
         xhr.setRequestHeader("Content-type", "application/json");
         xhr.onreadystatechange = function () {
             if (xhr.readyState === 4 && xhr.status === 200) {
-                importStudent();
+                drawtable();
             }
         };
         xhr.send(JSON.stringify(requestBody));
@@ -197,11 +414,11 @@ function deleteStudent() {
     var requestBody = {
         id: this.id.substring(9, this.id.length)
     };
-    xhr.open("POST", `${constants.server}delete-student`, true);
+    xhr.open("POST", `${constants.server}delete-student`, false);
     xhr.setRequestHeader("Content-type", "application/json");
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
-            importStudent();
+            drawtable();
         }
     };
     xhr.send(JSON.stringify(requestBody));
